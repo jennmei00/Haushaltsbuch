5d2c1414c0b5ce09a7eace501a28ff3c554576df
jessna96 <jessica-meissner@t-online.de>
2021-08-31 21:38:42 +0200

    edited account screen

--- lib/screens/account/account_screen.dart
+++ lib/screens/account/account_screen.dart
@@ -7,11 +7,65 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.blue[200],
+      child: Icon(Icons.add),
+      onPressed: () {},
+      ),
     appBar: AppBar(
       title: Text('Konten'),
       centerTitle: true,
       backgroundColor: Theme.of(context).primaryColor
     ),
     drawer: AppDrawer(),
+    body: Padding(
+      padding: const EdgeInsets.all(30),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        alignment: Alignment.center,
+        child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Vermögen:\n10.000€',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+                
+                ),
+            ),
+            SingleChildScrollView(
+              child:
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Card(
+                      child: ExpansionTile(
+                        initiallyExpanded: false,
+                        title: Text('Kontokategorie 1'),
+                        children: [
+                          Card(
+                            margin: EdgeInsets.all(10),
+                            color: Colors.lightBlue,
+                            child: ListTile(
+                              title: Text('Test'),
+                            ),
+                          ),
+                          Card(
+                            margin: EdgeInsets.all(10),
+                            color: Colors.lightBlue,
+                            child: ListTile(
+                              title: Text('Test 2'),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+            )
+          ],),
+      ),
+    )
   );
 }
