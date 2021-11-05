3209ce52fc131516df5c4f1964976c969c5401b6
jessna96 <jessica-meissner@t-online.de>
2021-11-05 09:39:30 +0100

    added images for posting

--- lib/screens/posting/posting_screen.dart
+++ lib/screens/posting/posting_screen.dart
@@ -7,7 +7,9 @@ class PostingScreen extends StatelessWidget {
   static final routeName = '/posting_screen';
   final urlBackgroundImageIncome =
       'https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80';
-
+  final urlBackgroundImageExpense = 'https://images.unsplash.com/photo-1567427017947-545c5f8d16ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1612&q=80';
+  final urlBackroundImageTransfer = 'https://images.unsplash.com/photo-1523629619140-ee5b56cb3b23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3114&q=80';
+  
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -26,8 +28,8 @@ class PostingScreen extends StatelessWidget {
                     _income(context);
                   },
                   child: Container(
-                    padding: EdgeInsets.all(30),
-                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(25),
+                    margin: EdgeInsets.all(25),
                     width: double.infinity,
                     //height: double.infinity,
                     decoration: BoxDecoration(
@@ -37,69 +39,68 @@ class PostingScreen extends StatelessWidget {
                         fit: BoxFit.cover,
                       ),
                     ),
-                    child: Text('Einnahme'),
-                    // child: Padding(
-                    //   padding: const EdgeInsets.all(30),
-                    // child: FractionallySizedBox(
-                    //   widthFactor: 1,
-                    //     child: OutlinedButton(
-                    //       onPressed: () {
-                    //         _income(context);
-                    //       },
-                    //       child: Text('Einnahme'),
-                    //       style: ButtonStyle(
-                    //         shape:
-                    //             MaterialStateProperty.all<RoundedRectangleBorder>(
-                    //           RoundedRectangleBorder(
-                    //             borderRadius: BorderRadius.circular(18.0),
-                    //           ),
-                    //         // ),
-                    //       // ),
-                    //     ),
-                    //   ),
-                    // ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _expense(context);
-                      },
-                      child: Text('Ausgabe'),
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                          ),
-                        ),
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      'Einnahme',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
                       ),
                     ),
                   ),
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _transfer(context);
-                      },
-                      child: Text('Umbuchung'),
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                          ),
-                        ),
+                child: GestureDetector(
+                  onTap: () {
+                    _income(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(25),
+                    margin: EdgeInsets.all(25),
+                    width: double.infinity,
+                    //height: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                        image: NetworkImage(urlBackgroundImageExpense),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      'Ausgabe',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    _income(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(25),
+                    margin: EdgeInsets.all(25),
+                    width: double.infinity,
+                    //height: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                        image: NetworkImage(urlBackroundImageTransfer),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      'Umbuchung',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
                       ),
                     ),
                   ),
